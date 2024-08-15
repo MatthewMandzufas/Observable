d540d074034cd305522820b2676ceb144934591d
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-15 12:49:41 +0800

    GREEN: Observable > should send errors thrown in the constructor down the error path

--- src/new-observable.ts
+++ src/new-observable.ts
@@ -1,18 +1,53 @@
-type Observer<T> = {
-  next: (value: T) => void;
-  error?: () => void;
-  complete: () => void;
+type NextObserver<T> = {
   closed?: boolean;
+  next: (value: T) => void;
+  error?: (err: any) => void;
+  complete?: () => void;
 };
 
-export default class Observable<T> {
+type ErrorObserver<T> = {
+  closed?: boolean;
+  next?: (value: T) => void;
+  error: (err: any) => void;
+  complete?: () => void;
+};
+
+type CompletionObserver<T> = {
+  closed?: boolean;
+  next?: (value: T) => void;
+  error?: (err: any) => void;
+  complete: () => void;
+};
+
+type PartialObserver<T> = NextObserver<T> | ErrorObserver<T> | CompletionObserver<T>;
+
+type Observer<T> = {
+  next: (value: T) => void;
+  error: (err: any) => void;
+  complete: () => void;
+  closed: boolean;
+};
+
+interface ObservableInterface<T> {
+  subscribe: (observer: Partial<Observer<T>>) => void;
+}
+
+export default class Observable<T> implements ObservableInterface<T> {
   #fnThatIsCalledWithObserver: (observer: Observer<T>) => void;
 
   constructor(fnThatIsCalledWithObserver: (observer: Observer<T>) => void) {
-    this.#fnThatIsCalledWithObserver = fnThatIsCalledWithObserver;
+    this.#fnThatIsCalledWithObserver = (observer: Observer<T>) => {
+      try {
+        fnThatIsCalledWithObserver(observer);
+      } catch (error) {
+        if (observer && typeof observer.error === 'function') {
+          observer?.error(error);
+        }
+      }
+    };
   }
 
-  subscribe({ next, complete }: Observer<T>) {
-    this.#fnThatIsCalledWithObserver({ next, error: () => {}, complete, closed: true });
+  subscribe({ next = () => {}, complete = () => {}, error = () => {}, closed = false }: Partial<Observer<T>>) {
+    this.#fnThatIsCalledWithObserver({ next, error, complete, closed });
   }
 }
